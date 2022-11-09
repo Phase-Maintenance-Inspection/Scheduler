e00dd0e5aca8c7e2ac3e1b6a5b00f53981fa146a
kerensaa <kerensaa@stud.ntnu.no>
2022-11-08 19:08:27 -1000

    add Popover when clicking lane name

--- src/ResourceView.js
+++ src/ResourceView.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {PropTypes} from 'prop-types'
 import Icon from 'antd/lib/icon'
+import Popover from 'antd/lib/popover'
 
 class ResourceView extends Component {
 
@@ -21,9 +22,13 @@ class ResourceView extends Component {
         const {schedulerData, contentScrollbarHeight, slotClickedFunc, slotItemTemplateResolver, toggleExpandFunc} = this.props;
         const {renderData, config} = schedulerData;
 
+        // const [open, setOpen] = useState(false);
+
         let width = schedulerData.getResourceTableWidth() - 2;
         let paddingBottom = contentScrollbarHeight;
         let displayRenderData = renderData.filter(o => o.render);
+        // var modal = document.getElementById("changeLaneModal");
+        // var span = document.getElementsByClassName("close")[0];
 
         let resourceList = displayRenderData.map((item) => {
             let bgColor = config.defaultEventBgColor;
@@ -53,12 +58,21 @@ class ResourceView extends Component {
             }
             indents.push(indent);
 
+            // const handleOpenChange = (newOpen) => {
+            //     setOpen(newOpen);
+            //   };
+
+            // const closeModal= () => {
+            //     modal.toggle = "none";
+            // };
+
             let a = slotClickedFunc != undefined ? <span className="slot-cell">{indents}<a className="slot-text" style={{width: width}} onClick={() => {
                 slotClickedFunc(schedulerData, item);
             }}>{item.slotName}</a></span>
                 : <span className="slot-cell">{indents}<span className="slot-text header4-text overflow-text" style={{width: width - 80}}>{item.slotName}</span></span>;
             let slotItem = (
-                <button style={{width: width}}>
+                <Popover content={content} title="Title" trigger="click">
+                <button style={{width: width}}  id='ChangeLaneButton'>
                     <div className="overflow-text header2-text" style={{textAlign: "left", color: '#F08421', fontSize: '14px', marginBottom:'15px'}}>
                          {item.slotIssue ? item.slotIssue : ''}
                     </div>
@@ -69,6 +83,8 @@ class ResourceView extends Component {
                          <span className=' overflow-text header3-text'>Assignee:</span> {item.slotAuthor ? item.slotAuthor : 'Unassignee'}
                     </div>
                 </button>
+                </Popover>
+                
                  
             );
 
@@ -77,6 +93,35 @@ class ResourceView extends Component {
                 if(!!temp)
                     slotItem = temp;
             }
+
+            const content = (
+                <div>
+                  <p>Content</p>
+                  <p>Content</p>
+                </div>
+              );
+
+            // let pop = (
+            //     <Popover
+            //         content={<a onClick={hide}>Close</a>}
+            //         title="Title"
+            //         trigger="click"
+            //         open={open}
+            //         onOpenChange={handleOpenChange}
+            //         >
+            //         <Button type="primary">Click me</Button>
+            //     </Popover>
+
+            // );
+
+            // let laneModal = (
+            //     <Popover id="changeLaneModal" placement="right">
+            //         <div class="modal-content">
+            //             <span class="close" onClick={() => {closeModal();}}>&times;</span>
+            //             <p>Some text in the Modal..</p>
+            //         </div>
+            //     </Popover>
+            // );
 
             let tdStyle = { height: item.rowHeight, display: 'inline-block' };
             if(item.groupOnly) {
